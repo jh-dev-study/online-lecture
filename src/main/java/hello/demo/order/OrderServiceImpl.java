2bb71ff009b347233bedac5d96d04b55693043ae
JuHyun419 <zzang9ha@naver.com>
2020-10-25 20:28:42 +0900

    Initial commit

---
+++ src/main/java/hello/demo/order/OrderServiceImpl.java
@@ -0,0 +1,32 @@
+package hello.demo.order;
+
+import hello.demo.discount.DiscountPolicy;
+import hello.demo.member.Member;
+import hello.demo.member.MemberRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
+
