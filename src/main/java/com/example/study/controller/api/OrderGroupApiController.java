503348b8bc6d16bb42d626ed545c16419d1a8377
JuHyun419 <zzang9ha@naver.com>
2020-12-17 22:14:01 +0900

    [Refactoring] add abstract Controller - CrudController

--- src/main/java/com/example/study/controller/api/OrderGroupApiController.java
+++ src/main/java/com/example/study/controller/api/OrderGroupApiController.java
@@ -1,16 +1,17 @@
 package com.example.study.controller.api;
 
-import com.example.study.ifs.CrudInterface;
-import com.example.study.model.network.Header;
+import com.example.study.controller.CrudController;
 import com.example.study.model.network.request.OrderGroupApiRequest;
 import com.example.study.model.network.response.OrderGroupApiResponse;
 import com.example.study.service.OrderGroupApiLogicService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/orderGroup")
-public class OrderGroupApiController implements
-        CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+public class OrderGroupApiController extends
+        CrudController<OrderGroupApiRequest, OrderGroupApiResponse> {
 
     private final OrderGroupApiLogicService orderGroupApiLogicService;
 
@@ -18,29 +19,34 @@ public class OrderGroupApiController implements
         this.orderGroupApiLogicService = orderGroupApiLogicService;
     }
 
-    @Override
-    @PostMapping("")
-    public Header<OrderGroupApiResponse> create(
-            @RequestBody  Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.create(request);
+    @PostConstruct
+    public void init() {
+        this.baseService = orderGroupApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}")
-    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
-        return orderGroupApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<OrderGroupApiResponse> update(
-            @RequestBody Header<OrderGroupApiRequest> request) {
-        return orderGroupApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return orderGroupApiLogicService.delete(id);
-    }
+//    @Override
+//    @PostMapping("")
+//    public Header<OrderGroupApiResponse> create(
+//            @RequestBody  Header<OrderGroupApiRequest> request) {
+//        return orderGroupApiLogicService.create(request);
+//    }
+//
+//    @Override
+//    @GetMapping("{id}")
+//    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
+//        return orderGroupApiLogicService.read(id);
+//    }
+//
+//    @Override
+//    @PutMapping("")
+//    public Header<OrderGroupApiResponse> update(
+//            @RequestBody Header<OrderGroupApiRequest> request) {
+//        return orderGroupApiLogicService.update(request);
+//    }
+//
+//    @Override
+//    @DeleteMapping("{id}")
+//    public Header delete(@PathVariable Long id) {
+//        return orderGroupApiLogicService.delete(id);
+//    }
 }
--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -1,17 +1,18 @@
 package com.example.study.controller.api;
 
-import com.example.study.ifs.CrudInterface;
-import com.example.study.model.network.Header;
+import com.example.study.controller.CrudController;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
 import com.example.study.service.UserApiLogicService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+public class UserApiController extends CrudController<UserApiRequest, UserApiResponse> {
 
     private final UserApiLogicService userApiLogicService;
 
@@ -19,29 +20,10 @@ public class UserApiController implements CrudInterface<UserApiRequest, UserApiR
         this.userApiLogicService = userApiLogicService;
     }
 
-    @Override
-    @PostMapping("")    // /api/user
-    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
-        log.info("{} , {}", request, "ABC");
-        return userApiLogicService.create(request);
+    @PostConstruct
+    public void init() {
+        this.baseService = userApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}")     // /api/user/{id}
-    public Header<UserApiResponse> read(@PathVariable(name = "id") Long id) {
-        log.info("read id : {}", id);
-        return userApiLogicService.read(id);
-    }
 
-    @Override
-    @PutMapping("")     // /api/user
-    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
-        return userApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")  // /api/user/{id}
-    public Header delete(@PathVariable Long id) {
-        return userApiLogicService.delete(id);
-    }
 }
